051ae97a5e7be86ca62fd14fdaa40593c4dfb1b5
Shashank Neelagiri <sneelagiri@outlook.com>
2020-02-05 19:44:40 +0100

    added players and linked to team

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const databaseUrl =
   process.env.DATABASE_URL ||
   "postgres://postgres:secret@localhost:5432/postgres";
 const db = new Sequelize(databaseUrl);
-db.sync()
+db.sync({ force: true })
   .then(() => {
     console.log("Schema will be updated here");
   })
