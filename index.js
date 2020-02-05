1985990b3da899b185219585c406a9bd2f9d2b98
Shashank Neelagiri <sneelagiri@outlook.com>
2020-02-05 17:42:24 +0100

    adding schema/model

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 const db = require("./db");
+const Team = require("./team/model");
 app.listen(port, () => {
   console.log("Started port on: ", port);
 });
